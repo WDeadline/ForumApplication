85366a98437d212e8497c37d62c335353589c5e0
Nguyễn Thị Bích Ngọc <35924978+Aiko419@users.noreply.github.com>
2018-12-09 03:52:38 +0700

    update api work

---
+++ ForumApi/ForumApi/Controllers/RecruitmentsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForumApi.Interfaces;
+using ForumApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ForumApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecruitmentsController : ControllerBase
+    {
+        private readonly ILogger<RecruitmentsController> _logger;
+        private readonly IRepository<Recruitment> _recruitmentRepository;
+
+        public RecruitmentsController(ILogger<RecruitmentsController> logger, IRepository<Recruitment> recruitmentRepository)
+        {
+            _logger = logger;
+            _recruitmentRepository = recruitmentRepository;
+        }
+        // GET: api/recruitments
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var recruitments = await _recruitmentRepository.GetAll();
+            return new OkObjectResult(recruitments);
+        }
+
+        // GET: api/recruitments/id
+        [HttpGet("{id:length(24)}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var recruitment = await _recruitmentRepository.GetById(id);
+            if (recruitment == null)
+                return new NotFoundResult();
+            return new OkObjectResult(recruitment);
+        }
+
+        // PUT: api/recruitments/id
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Put(string id, [FromBody]Recruitment recruitment)
+        {
+            var recruitmentFromDb = await _recruitmentRepository.GetById(id);
+            if (recruitmentFromDb == null)
+                return new NotFoundResult();
+            recruitmentFromDb.Place = recruitment.Place;
+            recruitmentFromDb.EndTime = recruitment.EndTime;
+            recruitmentFromDb.Title = recruitment.Title;
+            recruitmentFromDb.Tags = recruitment.Tags;
+            recruitmentFromDb.StartTime = recruitment.StartTime;
+            recruitmentFromDb.Description = recruitment.Description;
+            recruitmentFromDb.UpdationTime = DateTime.UtcNow;
+            _recruitmentRepository.Update(recruitmentFromDb);
+            return new OkResult();
+        }
+
+        // POST: api/recruitments
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Recruitment recruitment)
+        {
+            var result = _recruitmentRepository.Add(recruitment);
+            return new OkObjectResult(result);
+        }
+
+        // DELETE: api/recruitments/5
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var recruitmentFromDb = await _recruitmentRepository.GetById(id);
+            if (recruitmentFromDb == null)
+                return new NotFoundResult();
+            await _recruitmentRepository.Delete(id);
+            return new OkResult();
+        }
+    }
+}
---
+++ ForumApi/ForumApi/Controllers/WorksController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForumApi.Interfaces;
+using ForumApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ForumApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorksController : ControllerBase
+    {
+        private readonly ILogger<WorksController> _logger;
+        private readonly IRepository<Work> _workRepository;
+
+        public WorksController(ILogger<WorksController> logger, IRepository<Work> workRepository)
+        {
+            _logger = logger;
+            _workRepository = workRepository;
+        } 
+        // GET: api/Works
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var works = await _workRepository.GetAll();
+            return new OkObjectResult(works);
+        }
+
+        // GET: api/Works/id
+        [HttpGet("{id:length(24)}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var Work = await _workRepository.GetById(id);
+            if (Work == null)
+                return new NotFoundResult();
+            return new OkObjectResult(Work);
+        }
+
+        // PUT: api/Works/id
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Put(string id, [FromBody]Work work)
+        {
+            var workFromDb = await _workRepository.GetById(id);
+            if (workFromDb == null)
+                return new NotFoundResult();
+            workFromDb.Position = work.Position;
+            workFromDb.Salary = work.Salary;
+            workFromDb.Title = work.Title;
+            workFromDb.Tags = work.Tags;
+            workFromDb.Address = work.Address;
+            workFromDb.Description = work.Description;
+            workFromDb.UpdationTime = DateTime.UtcNow;
+            _workRepository.Update(workFromDb);
+            return new OkResult();
+        }
+
+        // POST: api/Works
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Work work)
+        {
+            var result = _workRepository.Add(work);
+            return new OkObjectResult(result);
+        }
+
+        // DELETE: api/Works/5
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var workFromDb = await _workRepository.GetById(id);
+            if (workFromDb == null)
+                return new NotFoundResult();
+            await _workRepository.Delete(id);
+            return new OkResult();
+        }
+    }
+}
